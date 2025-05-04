73467e7a22cbd43c998786846c3068a516f1fcf2
yulmkao <yulmkao@gmail.com>
2025-05-04 21:54:09 +0300

    7.2 Создание объекта

---
+++ src/main/java/ru/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
