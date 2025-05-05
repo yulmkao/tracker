9da4aa744a4a0910c587c0b8904c128661b9a40f
yulmkao <yulmkao@gmail.com>
2025-05-05 18:57:31 +0300

    7.5 Вызов метода с возвращаемым типом

---
+++ src/main/java/ru/oop/DummyDic.java
@@ -0,0 +1,14 @@
+package ru.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic word = new DummyDic();
+        String result = word.engToRus("Hi");
+        System.out.println(result);
+    }
+}
