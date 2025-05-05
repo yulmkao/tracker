a75b0e3aff02c607a6df49eb3a35b160d29ad75d
yulmkao <yulmkao@gmail.com>
2025-05-05 21:55:37 +0300

    7.9 Перегрузка конструктора

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -4,6 +4,23 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +35,14 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        Item item2 = new Item("Goods");
+        Item item3 = new Item(3, "Cola");
+        item1.printInfo();
+        item2.printInfo();
+        item3.printInfo();
     }
 }
