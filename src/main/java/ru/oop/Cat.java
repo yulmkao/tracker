ba2021ed0f7358b9e4dc1b247d194ab4de335c89
yulmkao <yulmkao@gmail.com>
2025-05-05 19:07:38 +0300

    7.6 Поля объекта

---
+++ src/main/java/ru/oop/Cat.java
@@ -0,0 +1,34 @@
+package ru.oop;
+
+public class Cat {
+
+    private String food;
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gavgav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Blackie");
+        black.eat("fish");
+        black.show();
+    }
+}
