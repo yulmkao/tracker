af9130cc1ee570810f3f9b4427d4aa17fde0b76c
yulmkao <yulmkao@gmail.com>
2025-05-06 17:26:13 +0300

    7.14 Рефакторинг. Площадь треугольника

---
+++ src/test/java/ru/oop/TriangleTest.java
@@ -0,0 +1,30 @@
+package ru.oop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+public class TriangleTest {
+    @Test
+    public void when00and40and04Then8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 8;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when11and22and33ThenMinus1() {
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 2);
+        Point c = new Point(3, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+
+    }
+}
