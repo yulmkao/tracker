0b1b4838e85f4889a0a2c94437ba8df843633886
yulmkao <yulmkao@gmail.com>
2025-05-11 21:31:51 +0300

    8.6 Аннотация @Override

--- src/main/java/ru/inheritance/JSONReport.java
+++ src/main/java/ru/inheritance/JSONReport.java
@@ -1,7 +1,11 @@
 package ru.inheritance;
 
 public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + "\t\"name\" : \"" + name + "\"," + System.lineSeparator() + "\t\"body\" : \"" + body + "\"" + System.lineSeparator() + "}";
+        return "{" + System.lineSeparator() +
+                "\t\"name\" : \"" + name + "\"," + System.lineSeparator() +
+                "\t\"body\" : \"" + body + "\"" + System.lineSeparator() + "}";
     }
 }
