45f2a3c450b1416676eca566e08dbab86bfdd5ab
yulmkao <yulmkao@gmail.com>
2025-05-13 22:54:40 +0300

    9.7 Tracker - хранилице

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,21 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                items[count++] = item;
+            }
+        }
+        return Arrays.copyOf(items, count);
     }
 
     public Item findById(int id) {
