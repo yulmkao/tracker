cd841b4e4727d24f7423f6efb1d384a0cb895a3c
yulmkao <yulmkao@gmail.com>
2025-05-14 19:49:36 +0300

    9.8 Метод замены заявки. Tracker.replace

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -28,15 +28,29 @@ public class Tracker {
         return Arrays.copyOf(items, count);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
-                break;
+                return index;
             }
         }
-        return rsl;
+        return -1;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 }
