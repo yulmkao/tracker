99f0b2205edb9a16bc890aa58345b46d8f2a32bd
yulmkao <yulmkao@gmail.com>
2025-05-15 23:36:38 +0300

    10.3 Чтение числа из  консоли

---
+++ src/main/java/ru/io/Matches.java
@@ -0,0 +1,28 @@
+package ru.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Игра 11.");
+        boolean turn = true;
+        int count = 11;
+        while (count > 0) {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3:");
+            int matches = Integer.parseInt(input.nextLine());
+            if (1 <= matches && matches <= 3 && count >= matches) {
+                count -= matches;
+                if (count == 0) {
+                    System.out.println("Игрок победил");
+                } else {
+                    System.out.println("Передаем ход другому игроку");
+                    turn = !turn;
+                }
+            } else {
+                System.out.println("Ошибка. Введите число повторно");
+            }
+        }
+    }
+}
