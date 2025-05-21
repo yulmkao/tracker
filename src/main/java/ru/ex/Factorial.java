0f900c06ec4fa9b373c23f15c5f4a311b7f19a84
yulmkao <yulmkao@gmail.com>
2025-05-22 00:21:32 +0300

    11.3 throw new RuntimeException

---
+++ src/main/java/ru/ex/Factorial.java
@@ -0,0 +1,18 @@
+package ru.ex;
+
+public class Factorial {
+    public int calc(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number should not be less than zero");
+        }
+        int result = 1;
+        for (int index = 2; index <= number; index++) {
+            result *= index;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new Factorial().calc(-1);
+    }
+}
