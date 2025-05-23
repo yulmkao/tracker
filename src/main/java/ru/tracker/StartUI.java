26d7c8e238e89dbef295c6bcc38e508c2a16073e
yulmkao <yulmkao@gmail.com>
2025-05-23 18:40:37 +0300

    11_8_Обеспечить_бесперебойную_работу_приложения_Tracker

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.tracker;
 import ru.tracker.action.*;
 import ru.tracker.input.ConsoleInput;
 import ru.tracker.input.Input;
+import ru.tracker.input.ValidateInput;
 import ru.tracker.output.ConsoleOutput;
 import ru.tracker.output.Output;
 
@@ -18,6 +19,10 @@ public class StartUI {
         while (run) {
             showMenu(actions);
             int select = input.askInt("Выбрать: ");
+            if (select < 0 || select >= actions.length) {
+                output.println("Неверный ввод, вы можете выбрать: 0 .. " + (actions.length - 1));
+                continue;
+            }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -32,7 +37,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new Create(output),
---
+++ src/main/java/ru/tracker/input/ValidateInput.java
@@ -0,0 +1,14 @@
+package ru.tracker.input;
+
+public class ValidateInput extends ConsoleInput {
+
+    @Override
+    public int askInt(String question) {
+        try {
+            return super.askInt(question);
+        } catch (NumberFormatException e) {
+            System.out.println("Пожалуйста, введите корректные данные");
+        }
+        return -1;
+    }
+}
