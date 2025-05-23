d95b802f6c7e6393b33c9749eef1f193263483b2
yulmkao <yulmkao@gmail.com>
2025-05-23 22:53:22 +0300

    11_8_Обеспечить_бесперебойную_работу_приложения_Tracker

--- src/main/java/ru/tracker/input/ValidateInput.java
+++ src/main/java/ru/tracker/input/ValidateInput.java
@@ -4,11 +4,12 @@ public class ValidateInput extends ConsoleInput {
 
     @Override
     public int askInt(String question) {
-        try {
-            return super.askInt(question);
-        } catch (NumberFormatException e) {
-            System.out.println("Пожалуйста, введите корректные данные");
+        while (true) {
+            try {
+                return super.askInt(question);
+            } catch (NumberFormatException e) {
+                System.out.println("Пожалуйста, введите корректные данные");
+            }
         }
-        return -1;
     }
 }
